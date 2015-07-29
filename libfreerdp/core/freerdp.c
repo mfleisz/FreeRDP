de303b82abe6828809f91cc53b8ca7f17a52b400
Armin Novak <armin.novak@thincast.com>
2015-07-29 15:46:40 +0200

    Aborting transport layer now if requested.

--- libfreerdp/core/freerdp.c
+++ libfreerdp/core/freerdp.c
@@ -191,6 +191,9 @@ BOOL freerdp_abort_connect(freerdp* instance)
 	if (!instance || !instance->context)
 		return FALSE;
 
+	if (instance->context->rdp)
+		instance->context->rdp->disconnect = TRUE;
+
 	return SetEvent(instance->context->abortEvent);
 }
 
