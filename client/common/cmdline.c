7ef99cdd77bc6bd57424db9d35668a697d6e981f
Armin Novak <armin.novak@thincast.com>
2023-01-09 15:56:20 +0100

    [core] fix persistent bitmap cache setting
    * Only activate if both, client and server support the capability
    * Use settings getter to access setting

--- client/common/cmdline.c
+++ client/common/cmdline.c
@@ -3822,7 +3822,8 @@ int freerdp_client_settings_parse_command_line_arguments(rdpSettings* settings, 
 		CommandLineSwitchCase(arg, "persist-cache")
 		{
 			WLog_WARN(TAG, "/persist-cache is deprecated, use /cache:persist[:on|off] instead");
-			settings->BitmapCachePersistEnabled = enable;
+			if (!freerdp_settings_set_bool(settings, FreeRDP_BitmapCachePersistEnabled, enable))
+				return COMMAND_LINE_ERROR_UNEXPECTED_VALUE;
 		}
 		CommandLineSwitchCase(arg, "persist-cache-file")
 		{
@@ -3831,7 +3832,8 @@ int freerdp_client_settings_parse_command_line_arguments(rdpSettings* settings, 
 			if (!freerdp_settings_set_string(settings, FreeRDP_BitmapCachePersistFile, arg->Value))
 				return COMMAND_LINE_ERROR_MEMORY;
 
-			settings->BitmapCachePersistEnabled = TRUE;
+			if (!freerdp_settings_set_bool(settings, FreeRDP_BitmapCachePersistEnabled, TRUE))
+				return COMMAND_LINE_ERROR_UNEXPECTED_VALUE;
 		}
 		CommandLineSwitchCase(arg, "offscreen-cache")
 		{
